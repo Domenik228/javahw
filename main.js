d241bbe58ece06b2802df3f6bf00234a11f82a94
Domenik228 <akbarbrawlstarsnick@gmail.com>
2025-02-10 15:49:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+let String = prompt("String typdagi matnni kiriting");
+
+
+console.log(String);
+
+
+
+let Son = prompt("Number typdagi matnni kiriting");
+
+
+console.log(Number(Son));
+
+
+let blooen = prompt("Boolean typdagi matnni kiriting");
+
+
+console.log(Boolean("true"));
+
+
+alert(`siz kiritgan ${String} string typega tegishli  \nsiz kiritgan ${Son}  number typega tegishli \nsiz kiritgan ${blooen} Boolean typega tegishli `  );
+
+
+
+
+
